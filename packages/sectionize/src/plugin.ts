ac4f01786d1675a75bdfee0cc391aedb940c77a1
Billy Kwok <xobkwok@gmail.com>
2020-08-05 12:59:59 +0800

    chore: update dependencies

--- packages/sectionize/src/plugin.ts
+++ packages/sectionize/src/plugin.ts
@@ -1,6 +1,5 @@
 import visit from 'unist-util-visit-parents';
 import deepmerge from 'deepmerge';
-
 import defaultOptions from './defaultOptions';
 import isFirstInSection from './isFirstInSection';
 import sectionize from './sectionize';
@@ -8,7 +7,7 @@ import sectionize from './sectionize';
 import type { Node, Parent } from 'unist';
 import type { Options } from './types';
 
-function plugin<T extends Node>(options = defaultOptions) {
+function plugin<T extends Node>(options = defaultOptions): (tree: T) => void {
   return (tree: T): void => {
     const { tagName, whitelist } = deepmerge.all<Options>([
       defaultOptions,
